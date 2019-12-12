0f215846b8eea04a2c34512430ba98391f6754f9
Jacob Ågård Bennike <jacob@regnekraft.dk>
2019-12-12 13:50:55 +0100

    helper and save methods implemented

--- services/Unic_ws17_service.cs
+++ services/Unic_ws17_service.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lib_ws17_service.WsiEksport;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,44 @@ using System.Threading.Tasks;
 
 namespace Lib_ws17_service.services
 {
-    class Unic_ws17_service
+    public class Unic_ws17_service
     {
+        public string signed_helloworld(string wsBrugerid, string wsPassword)
+        {
+            WsiEksportPortTypeClient s = new WsiEksportPortTypeClient();
+            helloWorldRequest req = new helloWorldRequest();
+            NoArgs noargs = new NoArgs();
+            Credentials creds = new Credentials();
+            creds.wsBrugerid = wsBrugerid;
+            creds.wsPassword = wsPassword;
+            var test = s.helloWorldWithCredentials(creds);
+            return test.helloWorldResult;
+        }
+
+        public void SaveEmployeesFromInstitution(string wsBrugerid, string wsPassword, string institution_nr)
+        {
+            List<string> employee_cprs = new List<string>();
+            WsiEksportPortTypeClient s = new WsiEksportPortTypeClient();
+            EksporterXml eksporterXml = new EksporterXml();
+            eksporterXml.wsBrugerid = wsBrugerid;
+            eksporterXml.wsPassword = wsPassword;
+            eksporterXml.instnr = institution_nr;
+            var test = s.eksporterXmlMellem(eksporterXml);
+
+            foreach (institutionPersonMedium p in test.xml.UNILoginExport.Institution.InstitutionPerson)
+            {
+                var name = p.UNILogin.Name;
+                var uniclogin = p.UNILogin.UserId;
+                var cpr = p.UNILogin.CivilRegistrationNumber;
+                var email = p.Person.EmailAddress;
+
+                if (employee_cprs.Contains(cpr))
+                {
+                    // DO STUFF
+                }
+            }
+
+
+        }
     }
 }
