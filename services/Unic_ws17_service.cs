1ec13bab748ae5d84fadf71ccdf79c4e3d6a8c0d
Jacob Ågård Bennike <jacob@regnekraft.dk>
2019-12-12 13:16:11 +0100

    ws17 wsdl added

---
+++ services/Unic_ws17_service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_ws17_service.services
+{
+    class Unic_ws17_service
+    {
+    }
+}
