4137ec0b4c034385fa90e6291cc9e1b0a6156eaa
Jacob Ågård Bennike <jacob@regnekraft.dk>
2020-01-09 11:00:53 +0100

    streamlining service

--- services/Unic_ws17_service.cs
+++ services/Unic_ws17_service.cs
@@ -1,4 +1,5 @@
-﻿using Lib_ws17_service.WsiEksport;
+﻿using Lib_ws17_service.dal;
+using Lib_ws17_service.WsiEksport;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,17 @@ namespace Lib_ws17_service.services
 {
     public class Unic_ws17_service
     {
-        public string signed_helloworld(string wsBrugerid, string wsPassword)
+        private string wsBrugerid;
+        private string wsPassword;
+        private string lora_constr;
+        public Unic_ws17_service(string wsBrugerid, string wsPassword, string lora_constr)
+        {
+            this.wsBrugerid = wsBrugerid;
+            this.wsPassword = wsPassword;
+            this.lora_constr = lora_constr;
+        }
+
+        public string signed_helloworld()
         {
             WsiEksportPortTypeClient s = new WsiEksportPortTypeClient();
             helloWorldRequest req = new helloWorldRequest();
@@ -21,30 +32,53 @@ namespace Lib_ws17_service.services
             return test.helloWorldResult;
         }
 
-        public void SaveEmployeesFromInstitution(string wsBrugerid, string wsPassword, string institution_nr)
+        private Dictionary<string, unic_username> Get_employees_from_UnicWS17()
         {
-            List<string> employee_cprs = new List<string>();
+            Institutions_repo repo = new Institutions_repo(lora_constr);
+            List<string> institution_nrs = repo.Query.Select(i => i.institution_nr).ToList<string>();
+            Position_repo pos_repo = new Position_repo(lora_constr);
+            Unic_usernames_repo unic_repo = new Unic_usernames_repo(lora_constr);
+            List<string> employee_cprs = pos_repo.Cprs;
+
             WsiEksportPortTypeClient s = new WsiEksportPortTypeClient();
             EksporterXml eksporterXml = new EksporterXml();
             eksporterXml.wsBrugerid = wsBrugerid;
             eksporterXml.wsPassword = wsPassword;
-            eksporterXml.instnr = institution_nr;
-            var test = s.eksporterXmlMellem(eksporterXml);
+            Dictionary<string, unic_username> res = new Dictionary<string, unic_username>();
 
-            foreach (institutionPersonMedium p in test.xml.UNILoginExport.Institution.InstitutionPerson)
+            foreach (string institution_nr in institution_nrs)
             {
-                var name = p.UNILogin.Name;
-                var uniclogin = p.UNILogin.UserId;
-                var cpr = p.UNILogin.CivilRegistrationNumber;
-                var email = p.Person.EmailAddress;
+                eksporterXml.instnr = institution_nr;
+                var test = s.eksporterXmlMellem(eksporterXml);
+                uniLoginExportMedium exm = test.xml.UNILoginExport;
 
-                if (employee_cprs.Contains(cpr))
+                if (exm.Institution != null)
                 {
-                    // DO STUFF
+                    if (exm.Institution.InstitutionPerson != null)
+                    {
+                        foreach (institutionPersonMedium p in test.xml.UNILoginExport.Institution.InstitutionPerson)
+                        {
+                            var name = p.UNILogin.Name;
+                            var uniclogin = p.UNILogin.UserId;
+                            var cpr = p.UNILogin.CivilRegistrationNumber;
+                            //var email = p.Person.EmailAddress;
+
+                            if (employee_cprs.Contains(cpr))
+                            {
+                                unic_username item = new unic_username() { cpr = cpr, unic_userid = uniclogin, institution_nr = institution_nr };
+                                res[uniclogin] = item;
+                                unic_repo.Add(item);
+                            }
+                        }
+                    }
                 }
             }
+            return res;
+        }
 
-
+        public void Save_and_update_UNICEmployees()
+        {
+            Dictionary<string, unic_username> unic_employees = Get_employees_from_UnicWS17()
         }
     }
 }
